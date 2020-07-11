02a7079a0912765ece76eba2b677c347e0373e01
AlbertoCoder97 <30498927+AlbertoCoder97@users.noreply.github.com>
2020-07-11 14:19:19 +0200

    Creating Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            GenerateButtons();
+        }
+
+        public void GenerateButtons()
+        {
+            //Create a 3x3 grid of buttons
+            Button[,] buttons = new Button[3, 3];
+
+            //Let's position the buttons
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    buttons[i,j] = new Button();
+                    //Size is the Object to create a Size and buttons[i,j] will have that size
+                    buttons[i, j].Size = new Size(100,100);
+                    //This allows us to define from which point the button gets drawn
+                    buttons[i, j].Location = new Point(i * 100, j * 100);
+                    //This will make our buttons look like a pane but in truth they are buttons.
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].TextAlign = ContentAlignment.MiddleCenter;
+
+                    //Let's choose a Font to make it look nice
+                    buttons[i, j].Font = new System.Drawing.Font(DefaultFont.FontFamily, 80, FontStyle.Bold);
+
+                    //Before adding the buttons to the panel now we gotta assign the method button_Click to the button event.
+                    //To add event handlers use +=
+                    buttons[i, j].Click += new EventHandler(button_Click);
+
+                    //This allows us to add the buttons to the Window
+                    panel1.Controls.Add(buttons[i,j]);
+
+                }
+            }
+        }
+
+        //This method allows us to control what happens when a button is clicked.
+        void button_Click(object sender, EventArgs e)
+        {
+            //Let's convert sender in button
+            Button button = sender as Button;
+
+            button.Text = "X";
+            
+        }
+    }
+}
